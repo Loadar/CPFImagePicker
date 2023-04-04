496343e2c7bd8256c8348088f42b84716ceb583b
ChenPengfei <afeiafeia@163.com>
2023-04-04 13:58:40 +0800

    更新CPFUIKit版本

--- Package.swift
+++ Package.swift
@@ -13,10 +13,8 @@ let package = Package(
             targets: ["CPFImagePicker"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/devxoul/Then", from: Version("3.0.0")),
-        //.package(url: "https://github.com/Loadar/CPFUIKit.git", from: Version("0.1.11")),
-        .package(url: "https://github.com/Loadar/CPFUIKit.git", branch: "master"),
+        .package(url: "https://github.com/Loadar/CPFUIKit.git", from: Version("0.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
